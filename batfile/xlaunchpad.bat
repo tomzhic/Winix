d3f64cfcb2fa145524e94cea00851f32c9f14fa2
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2015-11-28 08:12:28 +0000

    update
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

--- batfile/xlaunchpad.bat
+++ batfile/xlaunchpad.bat
@@ -7,3 +7,5 @@ mkdir %XLaunchPad_ROOT%
 copy %CYGWIN_ROOT%\xcfg\XLaunchPad.items.xml %XLaunchPad_ROOT%\AppData\Shortcuts\items.xml
 copy %CYGWIN_ROOT%\xcfg\XLaunchPad.Settings.ini %XLaunchPad_ROOT%\AppData\Settings.ini
 xcopy %SCRIPT_PATH%\icon %XLaunchPad_ROOT%\AppData\Shortcuts\Icons\Winix /e /y /c /i
+
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "XLaunchpad" /t REG_SZ /d "%WINIXROOT%\XLaunchPad\XLaunchpad.exe" /f >nul
