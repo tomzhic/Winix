3620f534d7e4d61846ce8a21b1a84aa4ffa7e7f7
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2015-12-21 12:03:29 +0000

    update
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

--- batfile/freecommander.bat
+++ batfile/freecommander.bat
@@ -8,3 +8,7 @@ mkdir %FreeCommander_ROOT%\SETTINGS
 copy %CYGWIN_ROOT%\xcfg\FreeCommander.fav.ini %FreeCommander_ROOT%\SETTINGS\FreeCommander.fav.ini
 copy %CYGWIN_ROOT%\xcfg\FreeCommander.ini %FreeCommander_ROOT%\SETTINGS\FreeCommander.ini
 copy %CYGWIN_ROOT%\xcfg\FreeCommander.shc %FreeCommander_ROOT%\SETTINGS\FreeCommander.shc
+
+
+reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\FreeCommander" /ve /t REG_SZ /d "FreeCommander" /f >nul
+reg add "HKEY_CLASSES_ROOT\Directory\Background\shell\FreeCommander\Command" /ve /t REG_SZ /d "\"%FreeCommander_ROOT%\FreeCommander.exe\" /T \"%%V\"" /f >nul
